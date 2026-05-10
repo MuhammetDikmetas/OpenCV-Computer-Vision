fa43f5056edcd9987df9b4243df604146fd70b28
Muhammet Dikmetaş <muhammetdikmetas756@gmail.com>
2026-05-10 14:57:05 +0300

    Görüntü okuma ve boyutlandırma çalışması eklendi
    OpenCV ile resim okuma, gri tonlama ve yeniden boyutlandırma işlemleri yapıldı.

---
+++ 01_resim_okuma_ornek.py
@@ -0,0 +1,25 @@
+import cv2
+ 
+orijinal_resim=cv2.imread("veri/sokak.jpg",0)
+
+yeni_resim=cv2.resize(orijinal_resim,(640,480))
+
+print(yeni_resim)
+
+cv2.namedWindow("resmim",cv2.WINDOW_NORMAL)
+
+cv2.imshow("resmim",yeni_resim)
+
+cv2.imwrite("veri/sokak1.jpg",yeni_resim)
+
+cv2.waitKey(0)
+
+
+cv2.destroyAllWindows() 
+
+
+
+
+
+
+
